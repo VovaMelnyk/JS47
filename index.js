dc4cbf7b2e808ba67005217ca58d34068368b4be
Vova Melnyk <hagmen4339@gmail.com>
2022-04-14 19:21:28 +0300

    add lesson 6 tasks

--- index.js
+++ index.js
@@ -178,118 +178,321 @@
 // в котором необходимо реализовать методы для работы с балансом и историей
 // транзакций.
 
-// ```js
-// /*
-//  * Типов транзацкий всего два.
-//  * Можно положить либо снять деньги со счета.
-//  */
-const Transaction = {
-  DEPOSIT: "deposit",
-  WITHDRAW: "withdraw",
-};
+// // ```js
+// // /*
+// //  * Типов транзацкий всего два.
+// //  * Можно положить либо снять деньги со счета.
+// //  */
+// const Transaction = {
+//   DEPOSIT: "deposit",
+//   WITHDRAW: "withdraw",
+// };
 
-// /*
-//  * Каждая транзакция это объект со свойствами: id, type и amount
-//  */
+// // /*
+// //  * Каждая транзакция это объект со свойствами: id, type и amount
+// //  */
 
-const account = {
-  // Текущий баланс счета
-  balance: 0,
+// const account = {
+//   // Текущий баланс счета
+//   balance: 0,
 
-  // История транзакций
-  transactions: [],
+//   // История транзакций
+//   transactions: [],
 
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
-  createTransaction(amount, type) {
-    // 135
-    return {
-      amount, // amount : 135
-      type, // type: 'deposit
-      id: this.transactions.length,
-    };
-  },
+//   /*
+//    * Метод создает и возвращает объект транзакции.
+//    * Принимает сумму и тип транзакции.
+//    */
+//   createTransaction(amount, type) {
+//     // 135
+//     return {
+//       amount, // amount : 135
+//       type, // type: 'deposit
+//       id: this.transactions.length,
+//     };
+//   },
 
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-  deposit(amount) {
-    // let amount = 135
-    this.balance += amount; // this.balance + 135
-    const transaction = this.createTransaction(amount, Transaction.DEPOSIT); // this.createTransaction(135, Transaction.DEPOSIT);
-    this.transactions.push(transaction);
-  },
+//   /*
+//    * Метод отвечающий за добавление суммы к балансу.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций
+//    */
+//   deposit(amount) {
+//     // let amount = 135
+//     this.balance += amount; // this.balance + 135
+//     const transaction = this.createTransaction(amount, Transaction.DEPOSIT); // this.createTransaction(135, Transaction.DEPOSIT);
+//     this.transactions.push(transaction);
+//   },
 
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  withdraw(amount) {
-    if (amount > this.balance) {
-      return "У Вас недостатньо коштів";
-    }
-    this.balance -= amount;
-    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
-    this.transactions.push(transaction);
-  },
+//   /*
+//    * Метод отвечающий за снятие суммы с баланса.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций.
+//    *
+//    * Если amount больше чем текущий баланс, выводи сообщение
+//    * о том, что снятие такой суммы не возможно, недостаточно средств.
+//    */
+//   withdraw(amount) {
+//     if (amount > this.balance) {
+//       return "У Вас недостатньо коштів";
+//     }
+//     this.balance -= amount;
+//     const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+//     this.transactions.push(transaction);
+//   },
 
-  /*
-   * Метод возвращает текущий баланс
-   */
-  getBalance() {
-    // let this = account
-    return this.balance;
-  },
+//   /*
+//    * Метод возвращает текущий баланс
+//    */
+//   getBalance() {
+//     // let this = account
+//     return this.balance;
+//   },
 
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  getTransactionDetails(id) {
-    for (let transaction of this.transactions) {
-      if (transaction.id === id) {
-        return transaction;
-      }
-    }
-    return "Такої операції не існує";
-  },
+//   /*
+//    * Метод ищет и возвращает объект транзации по id
+//    */
+//   getTransactionDetails(id) {
+//     for (let transaction of this.transactions) {
+//       if (transaction.id === id) {
+//         return transaction;
+//       }
+//     }
+//     return "Такої операції не існує";
+//   },
 
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-   */
-  getTransactionTotal(variant) {
-    let total = 0;
+//   /*
+//    * Метод возвращает количество средств
+//    * определенного типа транзакции из всей истории транзакций
+//    */
+//   getTransactionTotal(variant) {
+//     let total = 0;
 
-    for (let transaction of this.transactions) {
-      if (transaction.type === variant) {
-        total += transaction.amount;
-      }
-    }
-    return total;
-  },
-};
-// ```
+//     for (let transaction of this.transactions) {
+//       if (transaction.type === variant) {
+//         total += transaction.amount;
+//       }
+//     }
+//     return total;
+//   },
+// };
+// // ```
 
-console.log(account.getBalance());
-account.deposit(135);
-account.withdraw(50);
-account.withdraw(60);
-account.withdraw(10);
-account.deposit(200);
-account.deposit(2000);
-account.withdraw(500);
-console.log(account.getTransactionDetails(24));
-console.log(account);
-console.log(account.getTransactionTotal(Transaction.WITHDRAW));
+// console.log(account.getBalance());
+// account.deposit(135);
+// account.withdraw(50);
+// account.withdraw(60);
+// account.withdraw(10);
+// account.deposit(200);
+// account.deposit(2000);
+// account.withdraw(500);
+// console.log(account.getTransactionDetails(24));
+// console.log(account);
+// console.log(account.getTransactionTotal(Transaction.WITHDRAW));
 
 // transaction['type'] === transaction.type // 'deposit' 'withdraw'
+
+/////////////////////////////////
+
+// # Модуль 3 Занятие 6. Деструктуризация и rest/spread
+
+// ## Example 1 - Деструктуризация
+
+// Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
+// независимых аргументов.
+
+// ```js
+// function calcBMI(weight, height) {
+//   const numericWeight = Number(weight.replace(',', '.'));
+//   const numericHeight = Number(height.replace(',', '.'));
+//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// }
+
+// // Было
+// // console.log(calcBMI('88,3', '1.75'));
+// // console.log(calcBMI('68,3', '1.65'));
+// // console.log(calcBMI('118,3', '1.95'));
+
+// // Ожидается
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   }),
+// );
+// ```
+
+// ## Example 2 - Деструктуризация
+
+// Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
+// независимых аргументов.
+
+// ```js
+// function printContactsInfo(names, phones) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// // Было
+// // printContactsInfo(
+// //   'Jacob,William,Solomon,Artemis',
+// //   '89001234567,89001112233,890055566377,890055566300',
+// // );
+
+// // Ожидается
+// printContactsInfo({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
+// ```
+
+// ## Example 3 - Глубокая деструктуризация
+
+// Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
+// независимых аргументов.
+
+// ```js
+// function getBotReport(companyName, repairBots, defenceBots) {
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// // Было
+// // console.log(getBotReport('Cyberdyne Systems', 150, 50));
+
+// // Ожидается
+// console.log(
+//   getBotReport({
+//     companyName: 'Cyberdyne Systems',
+//     bots: {
+//       repair: 150,
+//       defence: 50,
+//     },
+//   }),
+// ); // "Cyberdyne Systems has 200 bots in stock"
+// ```
+
+// ## Example 4 - Деструктуризация
+
+// Перепиши функцию так, чтобы она принимала объект параметров со свойствами
+// `companyName` и `stock` и выводила репорт о количестве товаров на складе любой
+// компании.
+
+// ```js
+// // Решение
+// function getStockReport({ companyName, stock }) {
+//   let total = 0;
+//   for (const value of Object.values(stock)) {
+//     total += value;
+//   }
+//   return `${companyName} has ${total} items in stock`;
+// }
+
+// console.log(
+//   getStockReport({
+//     companyName: 'Cyberdyne Systems',
+//     stock: {
+//       repairBots: 150,
+//       defenceBots: 50,
+//     },
+//   }),
+// ); // "Cyberdyne Systems has 200 items in stock"
+
+// console.log(
+//   getStockReport({
+//     companyName: 'Belacci',
+//     stock: {
+//       shoes: 20,
+//       skirts: 10,
+//       hats: 5,
+//     },
+//   }),
+// ); // "Belacci has 35 item in stock"
+// ```
+
+// ## Example 5 - Операция spread
+
+// Дополни функцию `createContact(partialContact)` так, чтобы она возвращала новый
+// объект контакта с добавленными свойствами `id` и `createdAt`, а также `list` со
+// значением "default" если в `partialContact` нет такого свойства.
+
+// ```js
+// // Решение
+// function createContact(partialContact) {
+//   return {
+//     list: 'default',
+//     ...partialContact,
+//     id: generateId(),
+//     createdAt: Date.now(),
+//   };
+// }
+
+// console.log(
+//   createContact({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   }),
+// );
+// console.log(
+//   createContact({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   }),
+// );
+
+// function generateId() {
+//   return '_' + Math.random().toString(36).substr(2, 9);
+// }
+// ```
+
+// ## Example 6 - Операция rest
+
+// Напиши функцию `transformUsername(user)` так, чтобы она возвращала новый обьект
+// со свойством `fullName`, вместо `firstName` и `lastName`.
+
+// ```js
+// // Решение
+// function transformUsername({ firstName, lastName, ...otherProps }) {
+//   return {
+//     fullName: `${firstName} ${lastName}`,
+//     ...otherProps,
+//   };
+// }
+
+// console.log(
+//   transformId({
+//     id: 1,
+//     firstName: 'Jacob',
+//     lastName: 'Mercer',
+//     email: 'j.mercer@mail.com',
+//     friendCount: 40,
+//   }),
+// );
+
+// console.log(
+//   transformId({
+//     id: 2,
+//     firstName: 'Adrian',
+//     lastName: 'Cross',
+//     email: 'a.cross@hotmail.com',
+//     friendCount: 20,
+//   }),
+// );
+// ```
